97085019bcaf34fd9cbf13c42047860261509c4a
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2022-09-21 13:57:23 -0700

    Create Mutations.py

---
+++ Mutations.py
@@ -0,0 +1,14 @@
+def mutate_string(string, position, character):
+    string = list(string)
+    string[position]= character
+    new = ''
+    for x in string:
+        new = new+x
+    return new
+        
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
