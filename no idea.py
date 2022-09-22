84515dcbe0af9d7efae0d34ebb974fe13a5483a1
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2022-09-22 14:38:21 -0700

    Create no idea.py

---
+++ no idea.py
@@ -0,0 +1,11 @@
+size = map(int, input().split())
+array = map(int,input().split())
+a = set(map(int,input().split()))
+b = set(map(int,input().split()))
+happiness = 0
+for x in array:
+    if x in a:
+        happiness+=1
+    elif x in b:
+        happiness-=1
+print(happiness)
